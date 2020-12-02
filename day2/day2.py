2fdfc66093a004c1b4f2809dc1296024b0a28e03
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-12-02 00:48:52 -0500

    Day 2

---
+++ day2/day2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def is_valid_policyone(letter, min_, max_, password):
+    count = password.count(letter)
+    return (count >= min_) and (count <= max_)
+
+def at(s, x):
+    return s[x - 1: x]
+
+
+def is_valid_policytwo(letter, posa, posb, password):
+    return (at(password, posa) == letter) != (at(password, posb) == letter)
+
+
+num_valid_policyone = 0
+num_valid_policytwo = 0
+
+for line in open("input"):
+    policy, password = line.split(":")
+    ab, letter = policy.split(" ")
+    a, b = ab.split("-")
+    a = int(a)
+    b = int(b)
+    password = password.strip()
+
+    num_valid_policyone += (1 if is_valid_policyone(letter, a, b, password) else 0)
+    num_valid_policytwo += (1 if is_valid_policytwo(letter, a, b, password) else 0)
+
+print("Num valid policy one: %d" % num_valid_policyone)
+print("Num valid policy two: %d" % num_valid_policytwo)
